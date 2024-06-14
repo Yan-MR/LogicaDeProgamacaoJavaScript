a33f74f173b42bed3f95bae284ad88cb5504ec57
YanRodrigues <133587638+Yan-MR@users.noreply.github.com>
2024-06-14 10:21:40 -0300

    Update script.js

--- manzano/pag25/exercicio_A_pag25/script.js
+++ manzano/pag25/exercicio_A_pag25/script.js
@@ -3,4 +3,3 @@ let Fahrenheit
 Celsius=parseFloat(prompt("Digite a temperatura em Graus Celsius: "))
 Fahrenheit = (9 * Celsius + 160) / 5
 alert ("A temperatura de"+Celsius+"°C , convertida em Fahrenheit é de: "+ Fahrenheit+"F")
-Fimalgoritmo
